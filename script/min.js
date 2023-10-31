2ca824dfaf88ecbeef46b284483a5e22d78411a0
SIDDARTH-P-P <sidhusidharth2711@gmail.com>
2023-10-31 11:53:24 +0530

    snake created

--- script/min.js
+++ script/min.js
@@ -1,6 +1,30 @@
-let grid_box = document.getElementById("grid")
+let snakepos = 100;
+let grid_box = document.getElementById("grid");
 for(let i = 0 ; i<400; i++){
     let cell = document.createElement("div");
-    cell.classList.add("cell")
-    grid_box.appendChild(cell)
+    cell.classList.add("cell");
+    cell.setAttribute("id",i);
+    grid_box.appendChild(cell);
 }
+document.getElementById(snakepos).classList.add("snake")
+
+const eventHandler = (e) =>{
+    document.getElementById(snakepos).classList.remove("snake")
+    switch(e.key){ 
+    case "ArrowUp":
+        snakepos -= 20;
+    break;
+    case "ArrowDown":
+        snakepos += 20;
+    break;
+    case "ArrowLeft":
+        snakepos -= 1;
+    break;
+    case "ArrowRight":
+        snakepos += 1;
+    break;
+    }
+    document.getElementById(snakepos).classList.add("snake")
+}
+
+document.addEventListener("keydown",eventHandler)

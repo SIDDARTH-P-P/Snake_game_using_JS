34e11adc50b1af5a119bd832ffbd8d88864df0e7
SIDDARTH-P-P <sidhusidharth2711@gmail.com>
2023-11-08 12:37:59 +0530

    create food and adding snake body part after eating food

--- script/min.js
+++ script/min.js
@@ -1,30 +1,62 @@
-let snakepos = 100;
-let grid_box = document.getElementById("grid");
-for(let i = 0 ; i<400; i++){
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.setAttribute("id",i);
-    grid_box.appendChild(cell);
-}
-document.getElementById(snakepos).classList.add("snake")
+const playBoard = document.getElementById("play_board");
+let foodX , foodY ;
+let snakeX =5 , snakeY =20 ;
+let velocityX = 0 , velocityY = 0
+let snakeBody = [];
 
-const eventHandler = (e) =>{
-    document.getElementById(snakepos).classList.remove("snake")
-    switch(e.key){ 
-    case "ArrowUp":
-        snakepos -= 20;
-    break;
-    case "ArrowDown":
-        snakepos += 20;
-    break;
-    case "ArrowLeft":
-        snakepos -= 1;
-    break;
-    case "ArrowRight":
-        snakepos += 1;
-    break;
+
+const changefoodposition = ()=>{
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+
+let changedirection = (e) =>{
+    if(e.key === "ArrowLeft"){
+        velocityX = 0;
+        velocityY = -1;
+    }else if(e.key === "ArrowRight"){
+        velocityX = 0;
+        velocityY = 1;
+    }else if(e.key === "ArrowUp"){
+        velocityX = -1;
+        velocityY = 0;
+    }else if(e.key === "ArrowDown"){
+        velocityX = 1;
+        velocityY = 0;
     }
-    document.getElementById(snakepos).classList.add("snake")
+    initgame();
 }
 
-document.addEventListener("keydown",eventHandler)
+
+const initgame = ()=>{
+    let htmlMarkup = `<div class="food" style="grid-area:${foodX}/${foodY}"></div>`;
+
+
+    if(snakeX === foodX && snakeY === foodY){
+        changefoodposition();
+        snakeBody.push([foodX,foodY])
+        console.log(snakeBody);
+    }
+
+    for(i = snakeBody.length -1 ;i>0 ;i--){
+        snakeBody[i] = snakeBody[i-1]
+    }
+    snakeBody[0] = [snakeX,snakeY]
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    for(let i = 0 ; i< snakeBody.length;i++){ 
+        htmlMarkup += `<div class="head" style="grid-area:${snakeBody[i][0]}/${snakeBody[i][1]}"></div>`;
+    }
+    playBoard.innerHTML = htmlMarkup;
+}
+
+
+
+
+setInterval(initgame,500);
+document.addEventListener("keydown" , changedirection)
+
+changefoodposition();
+initgame();
